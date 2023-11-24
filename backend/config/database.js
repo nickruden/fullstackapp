bff42c89507e8b4c0c6f5c25d5e243039cb7a583
nickruden <1nikitos.ru2018@gmail.com>
2023-11-24 17:45:05 +0300

    database deploy config

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,10 +2,10 @@ import mysql from "mysql2";
   
 // create the connection to database
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'mydb'
+  host: 'bgof6x64lfwapxzdflwk-mysql.services.clever-cloud.com',
+  user: 'ul6ecpxcc34drwo7',
+  password: '79djLG1KeRpFRCcWGtpV',
+  database: 'bgof6x64lfwapxzdflwk'
 });
  
 export default db;
